04dc9f492e2ad74b727866b90375a559981a4375
Origami ONOUE <onoueorigami@gmail.com>
2020-01-24 00:54:57 +0900

    はい

---
+++ stylelint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    processors: ["stylelint-processor-styled-components"],
+    extends: [
+        "stylelint-config-recommended",
+        "stylelint-config-recess-order",
+        "stylelint-config-styled-components"
+    ]
+};
