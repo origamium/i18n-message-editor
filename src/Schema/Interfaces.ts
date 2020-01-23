10ba4f4bd3b87cebc00d77a71d2569234425a72a
Origami ONOUE <onoueorigami@gmail.com>
2020-01-24 02:42:47 +0900

    はい

--- src/Schema/Interfaces.ts
+++ src/Schema/Interfaces.ts
@@ -2,12 +2,14 @@ import { LanguageKeys } from "./LanguageKeys";
 
 export type LanguageKey = keyof typeof LanguageKeys;
 
+export type messageType = "plain" | "markdown" | "html";
+
 export interface i18nMessage {
     title: string;
     description: string;
     msg: {
         [key in LanguageKey]: {
-            isMarkdown: boolean;
+            type: messageType;
             text: string;
         };
     };
