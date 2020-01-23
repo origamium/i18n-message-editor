8131d9a6816d396f629b904c3e90fa5a65776e5b
Origami ONOUE <onoueorigami@gmail.com>
2020-01-24 01:59:42 +0900

    はい

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import styled from "styled-components";
+
+const Styled = {
+    Body: styled.main`
+        min-height: 100vh;
+    `
+};
 
 export const App: React.FC = () => {
-    return <div>{"はい"}</div>;
+    return <Styled.Body>{"はい"}</Styled.Body>;
 };
